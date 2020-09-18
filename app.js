d4b36b6cd536e8f8a1fda4ed5deed1e51f0a5f4b
renepromesse <nrenejordan1@gmail.com>
2020-09-18 15:46:03 +0200

    first file

---
+++ app.js
@@ -0,0 +1,49 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:4001"
+};
+
+app.use(cors(corsOptions));
+
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// a route to the root
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my application, where you can see all books I read till today." });
+});
+
+require("./app/routes/book.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
